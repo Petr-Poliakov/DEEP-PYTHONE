3ccca648b95d6d4208a9a55a9598703fb0c1723e
Petr-Poliakov <94320840+Petr-Poliakov@users.noreply.github.com>
2023-04-05 21:10:16 +0300

    Add files via upload

---
+++ __init__from DZ_7.py
@@ -0,0 +1,8 @@
+"""
+Соберите из созданных на уроке и в рамках домашнего задания функций пакет для работы с файлами.
+"""
+
+import os
+from pathlib import Path
+from DZ7task1 import rename_files 
+
