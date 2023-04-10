ad087635d3cc39d0c7eaa636d7b06511b47204dd
Petr-Poliakov <94320840+Petr-Poliakov@users.noreply.github.com>
2023-04-10 19:47:05 +0300

    Add files via upload

---
+++ __init__from DZ_8.py
@@ -0,0 +1,13 @@
+"""
+Соберите из созданных на уроке и в рамках домашнего задания функций пакет для работы с файлами.
+"""
+
+import os
+from pathlib import Path
+from DZ7task1 import rename_files
+import json
+import csv
+import pickle
+from dz_8 import save_directory_info
+
+
